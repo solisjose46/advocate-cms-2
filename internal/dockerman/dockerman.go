98bdbbfb851dec3a2ec7dc5c0be989c176a32807
Jose Salazar <solisjose466@gmail.com>
2024-03-26 04:19:40 +0000

    copytocontainer

--- internal/dockerman/dockerman.go
+++ internal/dockerman/dockerman.go
@@ -1,65 +1,88 @@
 package dockerman
 
 import (
-	"context"
-	"fmt"
+    "archive/tar"
+    "bytes"
+    "context"
+    "io"
+    "os"
+    "path/filepath"
 
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-	"github.com/docker/docker/client"
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/api/types/container"
+    "github.com/docker/docker/client"
 )
 
 func CreateAdvocateContainer() error {
-	// Create a new Docker client
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	if err != nil {
-		return err
-	}
+    ctx := context.Background()
+    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+    if err != nil {
+        return err
+    }
 
-	// Pull the Docker image (optional)
-	imageName := "golang:latest"
-	err = pullImage(cli, imageName)
-	if err != nil {
-		return err
-	}
+    // Create a buf to hold the tarred data to copy
+    buf := new(bytes.Buffer)
+    tw := tar.NewWriter(buf)
+    defer tw.Close()
 
-	// Define container configuration
-	containerConfig := &container.Config{
-		Image: imageName
-	}
+    // Walk the directory and tar each file
+    dirPath := "./advocate-2"
+    filepath.Walk(dirPath, func(file string, fi os.FileInfo, err error) error {
+        if err != nil {
+            return err
+        }
 
-	// Create the container
-	resp, err := cli.ContainerCreate(context.Background(), containerConfig, nil, nil, nil, "")
-	if err != nil {
-		return err
-	}
+        // create a new dir/file header
+        header, err := tar.FileInfoHeader(fi, file)
+        if err != nil {
+            return err
+        }
 
-	// Start the container
-	err = cli.ContainerStart(context.Background(), resp.ID, container.StartOptions{})
-	if err != nil {
-		return err
-	}
+        header.Name = filepath.ToSlash(file)
 
-	fmt.Printf("Container %s started successfully.\n", resp.ID)
+        // write the header
+        if err := tw.WriteHeader(header); err != nil {
+            return err
+        }
+
+        // if not a dir, write file content
+        if !fi.IsDir() {
+            data, err := os.Open(file)
+            if err != nil {
+                return err
+            }
+            if _, err := io.Copy(tw, data); err != nil {
+                return err
+            }
+            data.Close()
+        }
+        return nil
+    })
+
+    tw.Close()
+
+    // Use the Docker SDK to create and start a container
+    resp, err := cli.ContainerCreate(ctx, &container.Config{
+        Image: "golang:latest",
+        WorkingDir: "/app",
+        // Entrypoint:   []string{"go", "run", "/app/main.go"},
+    }, nil, nil, nil, "")
+    
+    if err != nil {
+        return err
+    }
+
+    // Copy the buffer to the container
+    err = cli.CopyToContainer(ctx, resp.ID, "/app", buf, types.CopyToContainerOptions{})
+        
+    if err != nil {
+        return err
+    }
+
+    // Start the container
+    if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+        return err
+    }
 
     return nil
-}
-
-func pullImage(cli *client.Client, imageName string) error {
-	out, err := cli.ImagePull(context.Background(), imageName, types.ImagePullOptions{})
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Print the pull status
-	buf := make([]byte, 1024)
-	for {
-		_, err := out.Read(buf)
-		if err != nil {
-			break
-		}
-	}
-
-	return nil
 }
